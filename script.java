96c95792c62c56c2da6d1ceae652b69d29fa7c99
VikasPavan09 <sunshinecreation43@gmail.com>
2026-06-05 22:43:40 +0530

    Update script.java

--- script.java
+++ script.java
@@ -1,15 +1,43 @@
-function yes() {
-  document.getElementById("proposal-content").style.display = "none";
-  document.getElementById("result").innerHTML =
-    "❤️ Nijam cheppali ante...\n\nNi love ki nen right person or kadhu ane vishayam nak thelidhu...\n\nBut I promise I will always stay with you ❤️";
-
-  document.getElementById("result").style.display = "block";
+function goMemory(){
+document.getElementById("birthday").classList.add("hidden");
+document.getElementById("memory").classList.remove("hidden");
 }
 
-function no() {
-  document.getElementById("proposal-content").style.display = "none";
-  document.getElementById("result").innerHTML =
-    "🌹 Pedhaga cheppaniki inkem ledhu...\n\nBut you will always be special to me ❤️";
+function goProposal(){
+document.getElementById("memory").classList.add("hidden");
+document.getElementById("proposal").classList.remove("hidden");
+}
 
-  document.getElementById("result").style.display = "block";
+/* YES */
+function showYes(){
+
+document.getElementById("question").style.display="none";
+document.getElementById("buttons").style.display="none";
+
+document.getElementById("result").innerHTML=`
+❤️ Nijam cheppali ante ni love ki nen right person or kadhu ane vishayam nak thelidhu.
+
+But I promise I will always stay with you ❤️
+
+Fights may happen... but love is forever.
+
+LOVE YOU VYSHUUUU ❤️
+— Vikas
+`;
+}
+
+/* NO */
+function showNo(){
+
+document.getElementById("question").style.display="none";
+document.getElementById("buttons").style.display="none";
+
+document.getElementById("result").innerHTML=`
+🌹 Pedhaga cheppaniki inkem ledhu...
+
+But you are still very special to me ❤️
+
+I will never forget you.
+— Vikas
+`;
 }
